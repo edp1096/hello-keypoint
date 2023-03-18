08e91b25480daded6588e305a407f93dff821daa
edp1096 <edp1096@naver.com>
2023-03-19 01:01:35 +0900

    oks

--- hello/oks.py
+++ hello/oks.py
@@ -7,27 +7,24 @@ def oks(y_true, y_pred, visibility):
     # You might want to set these global constant
     # outside the function scope
     KAPPA = np.array([1] * len(y_true))
+
     # The object scale
     # You might need a dynamic value for the object scale
     SCALE = 1.0
 
-    # Compute the L2/Euclidean Distance
-    distances = np.linalg.norm(y_pred - y_true, axis=-1)
-    # Compute the exponential part of the equation
-    exp_vector = np.exp(-(distances**2) / (2 * (SCALE**2) * (KAPPA**2)))
-    # The numerator expression
-    numerator = np.dot(exp_vector, visibility.astype(bool).astype(int))
-    # The denominator expression
-    denominator = np.sum(visibility.astype(bool).astype(int))
+    distances = np.linalg.norm(y_pred - y_true, axis=-1)  # Compute the L2/Euclidean Distance
+    exp_vector = np.exp(-(distances**2) / (2 * (SCALE**2) * (KAPPA**2)))  # Compute the exponential part of the equation
+    numerator = np.dot(exp_vector, visibility.astype(bool).astype(int))  # The numerator expression
+    denominator = np.sum(visibility.astype(bool).astype(int))  # The denominator expression
+
     return numerator / denominator
 
 
-if __name__ == "__main__":
-    IMAGE_SIZE_IN_PIXEL = 50
-    gt = (np.random.random((17, 2)) * IMAGE_SIZE_IN_PIXEL).astype(int)
-    pred = (np.random.random((17, 2)) * IMAGE_SIZE_IN_PIXEL).astype(int)
-    visibility = (np.random.random((17, 1)) * 3).astype(int)
+IMAGE_SIZE_IN_PIXEL = 50
+gt = (np.random.random((17, 2)) * IMAGE_SIZE_IN_PIXEL).astype(int)
+pred = (np.random.random((17, 2)) * IMAGE_SIZE_IN_PIXEL).astype(int)
+visibility = (np.random.random((17, 1)) * 3).astype(int)
 
-    # On this example the value will not be correct
-    # since you need to calibrate KAPPA and SCALE
-    print("OKS", oks(gt, pred, visibility))
+# On this example the value will not be correct
+# since you need to calibrate KAPPA and SCALE
+print("OKS", oks(gt, pred, visibility))
