968ac1c647bf41a20abd0c2883806a2d9efffb4a
edp1096 <edp1096@naver.com>
2023-03-26 22:58:32 +0900

    로딩

--- modules/dataset.py
+++ modules/dataset.py
@@ -12,24 +12,41 @@ class FacialKeypointsDataset(Dataset):
     def __init__(self, image_path, annotation_path, transforms=None):
         self.transforms = transforms
 
-        self.images = []
-        self.keypoints = []
+        # self.images = []
+        # self.keypoints = []
+        self.image_path = image_path
+        self.annotation_path = annotation_path
+        self.data_names = []
 
         for i, fname in enumerate(os.listdir(image_path)):
             fname_base = os.path.splitext(fname)[0]
 
-            # Load image
-            ipath = os.path.join(image_path, fname)
-            image = Image.open(ipath).convert("RGB")
-            self.images.append(image)
+            # Prepare names
+            data_name = {"image": fname, "keypoints": f"{fname_base}.csv"}
+            self.data_names.append(data_name)
 
-            # Load keypoints
-            kpath = os.path.join(annotation_path, f"{fname_base}.csv")
-            keypoints = np.genfromtxt(kpath, delimiter=",", dtype=np.float32)
-            self.keypoints.append(keypoints)
+            # # Load image
+            # ipath = os.path.join(image_path, fname)
+            # image = Image.open(ipath).convert("RGB")
+            # self.images.append(image)
+
+            # # Load keypoints
+            # kpath = os.path.join(annotation_path, f"{fname_base}.csv")
+            # keypoints = np.genfromtxt(kpath, delimiter=",", dtype=np.float32)
+            # self.keypoints.append(keypoints)
 
     def __getitem__(self, idx):
-        return_dict = {"image": self.images[idx], "keypoints": self.keypoints[idx][:POINT_NUM]}
+        # return_dict = {"image": self.images[idx], "keypoints": self.keypoints[idx][:POINT_NUM]}
+
+        # Load image
+        ipath = os.path.join(self.image_path, self.data_names[idx]["image"])
+        image = Image.open(ipath).convert("RGB")
+
+        # Load keypoints
+        kpath = os.path.join(self.annotation_path, self.data_names[idx]["keypoints"])
+        keypoints = np.genfromtxt(kpath, delimiter=",", dtype=np.float32)
+
+        return_dict = {"image": image, "keypoints": keypoints[:POINT_NUM]}
 
         if self.transforms:
             return_dict = self.transforms(return_dict)
@@ -37,4 +54,4 @@ class FacialKeypointsDataset(Dataset):
         return return_dict
 
     def __len__(self):
-        return len(self.images)
+        return len(self.data_names)
